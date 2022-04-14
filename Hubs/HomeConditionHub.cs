7f5bc34ebd4c791eac62e4903f61e5a2fcd4291d
mateuszlesko <mateusz.lesko@outlook.com>
2022-04-14 14:44:18 +0200

    FINISHED TEMPERATURE REAL TIME MONITOR

--- Hubs/HomeConditionHub.cs
+++ Hubs/HomeConditionHub.cs
@@ -8,21 +8,20 @@ namespace smartBuilding.Hubs;
 
 public class HomeConditionHub : Hub
 {
-    private static int _RefrashDelay = 30000;
+    private static const int _RefrashDelay = 5000;
     public async IAsyncEnumerable<HomeTemperature> RecentTemperature(CancellationToken stopToken)
     {
         while(! stopToken.IsCancellationRequested)
         {
             yield return HomeConditionRepository.Recent;
-        }
-
-        try
-        {
-            await Task.Delay(HomeConditionHub._RefrashDelay,stopToken);
-        }
-        catch(Exception)
-        {
-            yield break;
+            try
+            {
+                await Task.Delay(HomeConditionHub._RefrashDelay,stopToken);
+            }
+            catch(Exception)
+            {
+                yield break;
+            }
         }
     }
 }
